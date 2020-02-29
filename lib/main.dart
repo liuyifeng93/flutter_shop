1585e1911db28b1ed42956d6deae17b42a9b776c
刘一峰 <liuyifeng@tuhu.cn>
2020-02-29 23:08:38 +0800

    BottomNavigationBar init

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'pages/index_page.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: '百姓生活+',
+      theme: ThemeData(primaryColor: Colors.pink),
+      home: IndexPage(),
+    ));
+  }
+}
