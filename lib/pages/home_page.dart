a81b0b989fc238599bf0d3db61140543a2c28cde
刘一峰 <liuyifeng@tuhu.cn>
2020-03-01 00:42:52 +0800

    feat: swiper &&  screen adapter

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
-
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:flutter_shop/service/service_method.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
 
@@ -15,58 +18,47 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
-          appBar: AppBar(title: Text('美好人间')),
-          body: Container(
-              child: Column(
-            children: <Widget>[
-              TextField(
-                autofocus: false,
-                controller: _typeController,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(10.0),
-                  labelText: '美女类型',
-                  helperText: '请输入你喜欢的类型',
-                ),
-              ),
-              RaisedButton(
-                onPressed: _choiceAction,
-                child: Text('选择完毕'),
-              ),
-              Text(
-                showText,
-                overflow: TextOverflow.ellipsis,
-                maxLines: 1,
-              ),
-            ],
-          ))),
+        appBar: AppBar(title: Text('百姓生活+')),
+        body: FutureBuilder(
+          future: getHomePageContent(),
+          builder: (ctx, snapshot) {
+            if (snapshot.hasData) {
+              var data = json.decode(snapshot.data..toString());
+              List<Map> swiper = (data['data']['slides'] as List).cast();
+              print(swiper);
+              return Column(children: <Widget>[
+                SwiperDiy(swiperDataList: swiper)
+              ],);
+            } else {
+              return Center(child: Text('加载中'),);
+            }
+          },
+        ),
+      ),
     );
   }
-
-void _choiceAction() {
-  print('object');
-  if (_typeController.text.isEmpty) {
-    showDialog(
-      context: context, 
-      builder: (context)=>AlertDialog(title: Text("美女类型不能为空")));
-  } else {
-    final str = _typeController.text.toString();
-    getHttp(str).then((value){
-      setState(() {
-        showText = value['data']['name'].toString();
-      });
-    });
-  }
 }
-  Future getHttp(String url) async {
-    try {
-      Response response;
-      var data = {"name": url};
-      response = await Dio().get(
-          "https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian",
-          queryParameters: data);
-      return response.data;
-    } catch (e) {
-      return print(e);
-    }
+
+/// 首页轮播组件
+class SwiperDiy extends StatelessWidget {
+  const SwiperDiy({Key key, this.swiperDataList}) : super(key: key);
+  final List swiperDataList;
+
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.init(context, width: 375, height: 1334);
+    return Container(
+        height: ScreenUtil().setHeight(333),
+        width: ScreenUtil().setWidth(750),
+        child: Swiper(
+          pagination: SwiperPagination(),
+          autoplay: true,
+          itemCount: swiperDataList.length,
+          itemBuilder: (BuildContext ctx, int index) {
+            var url = swiperDataList[index]['image'];
+            print(url);
+            return Image.network(url, fit: BoxFit.fill,);
+          },
+        ));
   }
 }
